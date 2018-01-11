7424165e4bc20c7ee34a2024a70c7d2535b64789
Jun-Miyaoka <miyajun0220@gmail.com>
2018-01-11 10:26:00 +0900

    rdf修正

--- rdf/rdf.py
+++ rdf/rdf.py
@@ -12,6 +12,7 @@ import sys
 各時間のxy座標を配列にして返す
 """
 def fetchLine():
+
     allFiles = glob.glob('*.xvg')
     counter = 0
     for file in allFiles:
@@ -28,80 +29,57 @@ def fetchLine():
     return rdatas[range(1, tmax, dt),:]
 
 """
-各時間における粒子間距離を計算
-"""
-def getDistance(xydatas, particlesNum):
-    d = []
-    i = 0
-    for xy in xydatas:
-        for i,j in list(itertools.product(range(particlesNum), repeat=2)):
-            x1 = xy[i][0]
-            if (x1 > xsize):
-                x1 = x1 - xsize
-            x2 = xy[j][0]
-            if (x2 > xsize):
-                x2 = x2 - xsize
-            y1 = xy[i][1]
-            if (y1 > ysize):
-                y1 = y1 - ysize
-            y2 = xy[j][1]
-            if (y2 > ysize):
-                y2 = y2 - ysize
-            dx = np.absolute(x1-x2)
-            dy = np.absolute(y1-y2)
-            #周期境界条件除去
-            if float(dx)/xsize > 0.5:
-                dx = xsize - dx
-            if float(dy)/ysize > 0.5:
-                dy = ysize - dy
-            #距離計算
-            d.append(math.sqrt(dx**2 + dy**2))
-    return np.array(d).reshape(times, particlesNum**2)
-
-"""
 rdf計算
 """
-def calcGr(distanses, particlesNum, times):
-    allDensity = particlesNum / (math.pi * diag**2)
+def getRdf(xydatas, particlesNum, times):
 
+    hist = np.zeros((times, int(diag/dr)))
+    for time in range(0, times):
+        print 'calculate distanses time:', time
+        for i in range(0, particlesNum-1):
+            for j in range(i+1, particlesNum):
+                x1 = xydatas[time][i][0]
+                x2 = xydatas[time][j][0]
+                y1 = xydatas[time][i][1]
+                y2 = xydatas[time][j][1]
+                dx = (x1 - x2) - round((x1 - x2)/xsize)*xsize
+                dy = (y1 - y2) - round((y1 - y2)/ysize)*ysize
+                d = math.sqrt(dx**2 + dy**2)
+                hist[time][int(d/dr)] += 1
+    hist = np.delete(2 * hist, range(150,216), 1)
+    aveHist = np.mean(hist, axis=0) / particlesNum
+    print(len(aveHist))
     r = 0
     rd = []
     gr = []
-    while r < diag:
-        c = np.where((r < distanses) & (distanses <= (r + dr)))
-        n = len(c[0]) / (particlesNum * times)
-        if r == 0:
-            drDensity = n / (math.pi * dr**2)
-        else:
-            drDensity = n / (math.pi * 2 * r * dr)
-
+    allDensity = particlesNum / (xsize**2)
+    print 'get rdf'
+    for n in aveHist:
+        rInner = r
+        rOuter = r + dr
+        drDensity = n / (math.pi * (rOuter**2 - rInner**2))
         gr.append(drDensity / allDensity)
         rd.append(r)
         r += dr
 
-    return pd.Series(rd, gr)
+    return pd.Series(gr, rd)
 
 #main
 argv = sys.argv
-if len(argv) == 4:
-    xsize = 30.3483
-    ysize = 30.3483
-    diag  = 21.5
-    dr    = float(argv[1])
-    dt    = int(argv[2])
-    tmax  = int(argv[3])
+if len(argv) == 3:
+    xsize = 30.0
+    ysize = 30.0
+    diag  = 21.6
+    dr    = 0.1
+    dt    = int(argv[1])
+    tmax  = int(argv[2])
 else:
+    print("引数を入力してください")
     sys.exit()
 
-print 'start file import'
 xydatas = fetchLine()
-print 'end file import'
 particlesNum = xydatas.shape[1]
 times = xydatas.shape[0]
-print 'start calcurate distanse'
-distanses = getDistance(xydatas, particlesNum)
-print 'end calcurate distanse'
-print 'start calcurate rdf'
-gr = calcGr(distanses, particlesNum, times)
-print 'end calcurate rdf'
-gr.to_csv("rdf.csv")
+gr = getRdf(xydatas, particlesNum, times)
+print('end! check rdf.csv')
+# gr.to_csv("gr.csv")
